1f0e9151c65f9b3d1b3f1751dee1ca8e09c5f053
Alexander Krivács Schrøder <alexschrod@gmail.com>
2020-09-05 08:33:34 +0200

    Add time zone information to publish date

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -42,7 +42,7 @@ export default class BlogPostTemplate extends React.Component {
             <span
               title={moment(post.frontmatter.raw_date).format(
                 "MMMM DD, YYYY HH:mm"
-              )}
+              ) + " CE(S)T"}
             >
               {post.frontmatter.date}
             </span>{" "}
