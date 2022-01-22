b333d24e6096e7434ba2c95315e49c46a7f30936
Alexander Krivács Schrøder <alexschrod@gmail.com>
2022-01-22 21:50:28 +0100

    Update deploy location for new server

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ echo "${deploy_key}" > $HOME/.ssh/deploy_key
 chmod 600 $HOME/.ssh/deploy_key # SSH keys need to be readonly
 
 # Where to deploy our site on our server
-target="/var/www/html"
+target="/var/www/alexanderschroeder.net/www"
 
 # Grab exclusion file
 sh -c "rsync -azvh -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no' ${deploy_target}:${target}/protect-files ."
