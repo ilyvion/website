36015d741ba548234a6b30629ef5c777f067a08d
Alexander Krivács Schrøder <alexschrod@gmail.com>
2024-11-17 09:11:21 +0100

    chore: run formatter

--- src/components/ruby.tsx
+++ src/components/ruby.tsx
@@ -1,9 +1,16 @@
 import React from "react"
 
 export type RubyArgs = {
-    rt: string;
-    children: string;
-};
-export default function Ruby({rt, children}:RubyArgs) {
-    return <ruby>{children}<rp>&nbsp;(</rp><rt>{rt}</rt><rp>)</rp></ruby>
+  rt: string
+  children: string
+}
+export default function Ruby({ rt, children }: RubyArgs) {
+  return (
+    <ruby>
+      {children}
+      <rp>&nbsp;(</rp>
+      <rt>{rt}</rt>
+      <rp>)</rp>
+    </ruby>
+  )
 }
